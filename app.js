5c5b92753b30b7cce8fad511fcf33f1ab11ec7d2
Janagale <mmmwcr@gmail.com>
2018-09-06 11:06:25 +0300

    Animate form block

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var lname = document.getElementById('lname');
 var email = document.getElementById('email');
 var phone = document.getElementById('phone');
 var courses = document.getElementById('courses');
+var regForm = document.getElementById('regform');
+var joinBtn = document.getElementById('join');
 var btn = document.getElementById('register');
 var success = document.getElementById('success');
 var message = "<p class='text-success successmsg'> Something Went Wrong Please Try Later.</p>";
@@ -11,15 +13,25 @@ var errorMsg = "<p class='text-danger errormsg'> All fields are required.</p>";
 
 
 
-btn.addEventListener('click', function(){
-    
-    
-    
-    if(fname.value !== '' && lname.value !== '' && email.value !== '' && phone.value !== '' && courses.value !== ''){
-        
+btn.addEventListener('click', function () {
+
+
+
+    if (fname.value !== '' && lname.value !== '' && email.value !== '' && phone.value !== '' && courses.value !== '') {
+
         success.innerHTML = message;
     } else {
-        
+
         success.innerHTML = errorMsg;
     }
 });
+
+
+
+joinBtn.addEventListener('click', shakeForm);
+
+function shakeForm(e) {
+    e.preventDefault;
+    regForm.classList.toggle("slideInRight");
+    regForm.classList.toggle("shake");
+}
