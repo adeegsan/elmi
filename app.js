a9d7a0aeadac67bcf7cc1301091bb84a8e833c74
Janagale <mmmwcr@gmail.com>
2018-10-02 11:05:45 +0300

    add form validation and filtering.

--- app.js
+++ app.js
@@ -1,41 +1,66 @@
+var fname = document.getElementById('firstName');
+var lname = document.getElementById('lastName');
+var email = document.getElementById('email');
+var phone = document.getElementById('phone');
+var courses = document.getElementById('course');
+var regForm = document.getElementById('regform');
+var joinBtn = document.getElementById('join');
+var btn = document.getElementById('register');
+var success = document.getElementById('success');
+var message = "<p class='text-success successmsg'> Thanks for Registring we will contact you soon, Before Course start. </p>";
+var errorMsg = "<p class='text-danger errormsg'> All fields are required.</p>";
 
-//var fname = document.getElementById('fname');
-//var lname = document.getElementById('lname');
-//var email = document.getElementById('email');
-//var phone = document.getElementById('phone');
-//var courses = document.getElementById('courses');
-//var regForm = document.getElementById('regform');
-//var joinBtn = document.getElementById('join');
-//var btn = document.getElementById('register');
-//var success = document.getElementById('success');
-//var message = "<p class='text-success successmsg'> We are Working on the Online Registration System, Please Try Later. Thanks for Understanding. </p>";
-//var errorMsg = "<p class='text-danger errormsg'> All fields are required.</p>";
-//
-//
-//
-//
-//btn.addEventListener('click', function () {
-//
-//
-//
-//    if (fname.value !== '' && lname.value !== '' && email.value !== '' && phone.value !== '' && courses.value !== '') {
-//        
-//        
-//
-//        success.innerHTML = message;
-//    } else {
-//
-//        success.innerHTML = errorMsg;
-//    }
-//});
-//
-//
-//
-//joinBtn.addEventListener('click', shakeForm);
-//
-//function shakeForm(e) {
-//    e.preventDefault;
-//    regForm.classList.toggle("slideInRight");
-//    regForm.classList.toggle("shake");
-//    return false;
-//}
+
+
+
+//Function to submit data to google sheets.
+
+function insertDataToGoogleSheet() {
+    'use strict';
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxrgyG_pG6uqFUSu-ov5yUw_F-seSm6SsVaumfZz7jYLaWgkbI/exec';
+    const form = document.forms['submit-to-google-sheet'];
+
+
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => {
+            alert('Thanks: Registration completed Successfully.');
+            success.innerHTML = message;
+
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message));
+
+}
+
+
+
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+
+
+    if (fname.value !== '' && lname.value !== '' && email.value !== '' && phone.value !== '' && courses.value !== '') {
+
+        insertDataToGoogleSheet();
+
+    } else {
+
+        success.innerHTML = errorMsg;
+        return false;
+    }
+});
+
+
+
+function shakeForm(e) {
+    e.preventDefault;
+    regForm.classList.toggle("shake");
+
+}
+
+joinBtn.addEventListener('click', shakeForm);
